16e1ac81933750afdddba9eceb417302114ea84d
Fatbardh <fatbardh.kadriu@gmail.com>
2021-08-05 19:19:50 +0200

    api request validation

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { signup, signin, requireSignIn } = require('../controllers/auth')
+const { signup, signin }  = require('../controllers/auth')
+const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require('../validators/auth')
 
 const router = express.Router()
 
-router.post('/signin', signin)
-router.post('/signup', signup)
+router.post('/signin', validateSigninRequest, isRequestValidated, signin)
+router.post('/signup', validateSignupRequest, isRequestValidated, signup)
 
 module.exports = router;
