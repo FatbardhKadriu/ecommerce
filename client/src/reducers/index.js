804bfd114eef481fe9cb5a32a5986ec1035a3585
Fatbardh <fatbardh.kadriu@gmail.com>
2021-08-22 21:46:11 +0200

    front end got some style

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import categoryReducer from './category.reducer'
 import { combineReducers } from 'redux'
 import productReducer from './product.reducer'
+import authReducer from './auth.reducer'
 
 const rootReducer = combineReducers({
     category: categoryReducer,
-    product:  productReducer
+    product:  productReducer,
+    auth:     authReducer
 })
 
 export default rootReducer
